a416ba00ce752e1364a7c2b5c67750528792ca77
Joao_vap <joaovictorpimenta@gmail.com>
2021-11-04 12:36:01 -0300

    new simulation (with colors)

--- Caos_discreto/Mix_GoL_CA/mix.py
+++ Caos_discreto/Mix_GoL_CA/mix.py
@@ -7,13 +7,14 @@ from matplotlib.animation import FuncAnimation
 
 ########################### constants ##################################
 # grid size
-size = 100
+sizex = 500
+sizey = 50
 
-# colors
-cmap = colors.ListedColormap(['black', 'white'])
+# # colors
+# cmap = colors.ListedColormap(['black', 'white', 'red', 'blue', 'green', 'yellow', 'cyan', 'magenta'])
 
 #animation
-frames = 200
+frames = 500
 fps = 10
 
 ############################# classes ##################################
@@ -28,6 +29,8 @@ class block:
         self.i = i
         self.j = j
         self.state = state
+        self.color = state
+        self.desaturate = False
 
     def tanatos(self, grid):
         
@@ -43,7 +46,7 @@ class block:
                 if x == 0 and y == 0:
                     pass
                 else:
-                    if (0 <= (self.i + x) < size) and (0 <= (self.j + y) < size):
+                    if (0 <= (self.i + x) < sizey) and (0 <= (self.j + y) < sizex):
                         count += grid[self.i + x][self.j + y].state
                         
         # game conditions
@@ -70,7 +73,7 @@ class block:
             state = last_line[self.j-1].state
         else:
             state = int(not bool(last_line[self.j-1].state))
-
+        
         return state
 
 
@@ -80,22 +83,31 @@ class table:
     Table for organizing matriz that is used for controlling blocks
     '''
 
-    def __init__(self, matrix, N):
-        self.grid = [[block(i, j, matrix[i][j]) for j in range(0, N)] for i in range(0, N)]
+    def __init__(self, matrix, Nx, Ny):
+        self.grid = [[block(i, j, matrix[i][j]) for j in range(0, Nx)] for i in range(0, Ny)]
         self.last_line = self.grid[-1]
-        self.size = N
+        self.sizey = Ny
+        self.sizex = Nx
 
     def floats_matrix(self):
-        return [[float(self.grid[i][j].state) for j in range(0, self.size)] for i in range(0, self.size)]
+        return [[int(self.grid[i][j].color) for j in range(0, self.sizex)] for i in range(0, self.sizey)]
 
     def update_gol(self):
         '''
         update grid do decide next frame
         '''
         pivo_grid = deepcopy(self.grid)
-        for col in range(0, int(self.size*9/10)):
-            for lin in range(0, self.size):
-                self.grid[col][lin].state = pivo_grid[col][lin].tanatos(pivo_grid)
+        for col in range(0, int(self.sizey*9/10)):
+            for lin in range(0, self.sizex):
+                pivo = pivo_grid[col][lin].tanatos(pivo_grid)
+                self.grid[col][lin].state = pivo
+                if pivo == 1:
+                    self.grid[col][lin].color += 1
+                    self.grid[col][lin].desaturate = False
+                else:
+                    if self.grid[col][lin].color > 0:
+                        self.grid[col][lin].color /= 1.1
+
 
     def update_ca(self):
         '''
@@ -104,13 +116,16 @@ class table:
 
         pivo_line = deepcopy(self.last_line)
 
-        for lin in range(int(self.size*9/10), self.size-1):
+        for lin in range(int(self.sizey*9/10)-1, self.sizey-1):
             self.grid[lin] = deepcopy(self.grid[lin+1])
             
-        for col in range(1, self.size-1):
-            self.grid[size-1][col].state = pivo_line[col].elementar(pivo_line)
+        for col in range(1, self.sizex-1):
+            pivo = pivo_line[col].elementar(pivo_line)
+            self.grid[self.sizey-1][col].state = pivo
+            self.grid[self.sizey-1][col].color = pivo * 10
 
-        self.last_line = deepcopy(self.grid[size-1])
+
+        self.last_line = deepcopy(self.grid[self.sizey-1])
 
 ####################### auxiliar functions #############################
 
@@ -119,18 +134,24 @@ def read_init_state(file, N):
         matrix = [[int(num) for num in file.readline()[:-1].split(' ')] for i in range(0, N)]
     return matrix
 
+
+def malha(sizex, sizey):
+    aux = [[0 for i in range(0, sizex)] for j in range(0, int(sizey))]
+    aux[-1][int(len(aux[0])/2)] = 1
+    return aux
+
 ############################## main program ############################
 
 if __name__ == '__main__':
 
-    init = read_init_state("mix.txt", size)
-    got = table(init, size)
+    init = malha(sizex, sizey)
+    got = table(init, sizex, sizey)
     fig, ax = plt.subplots()
 
     def animate(i):
-        ax.imshow(got.floats_matrix(), cmap=cmap)
         got.update_ca()
+        ax.imshow(got.floats_matrix(), cmap="inferno", vmin=0, vmax=6)
         got.update_gol()
 
     anim = FuncAnimation(fig, animate, frames=frames)
-    anim.save('mix.gif', fps=fps)
+    anim.save('mix5.gif', fps=fps)
